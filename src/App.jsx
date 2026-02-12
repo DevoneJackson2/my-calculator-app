a5715e1c9af98ffa2beeade422d82170eb8bad94
DevoneJackson2 <devmjackson2002@gmail.com>
2026-02-12 18:38:05 -0500

    added more UI, fixed UI and added styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,47 +2,77 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [expression, setExpression] = useState("");
+  const [expression, SetExpression] = useState("");
+  const [result, SetResult] = useState("");
+
+  function handleChange(event)
+  {
+    SetExpression(event.target.value);
+    console.log(expression);
+  }
 
   return (
     <>
       <h1>My Calculator App</h1>
-      <input type="text" id="exprBox" placeholder='Please enter an expression...'></input>
+      <input type="text" id="exprBox" onChange={handleChange} value={expression} placeholder='Please enter an expression...'></input>
+      <p>{result}</p>
 
       <div id="buttons">
         <div id="operators">
-          <Button symbol="+" type="op"></Button>
-          <Button symbol="-" type="op"></Button>
-          <Button symbol="x" type="op"></Button>
-          <Button symbol="/" type="op"></Button>
-          <Button symbol="=" type="op res"></Button>
+          <OperatorButton symbol="+" type="op" expr={expression} SetExpr={SetExpression}/>
+          <OperatorButton symbol="-" type="op" expr={expression} SetExpr={SetExpression}/>
+          <OperatorButton symbol="X" type="op" expr={expression} SetExpr={SetExpression}/>
+          <OperatorButton symbol="/" type="op" expr={expression} SetExpr={SetExpression}/>
+          <OperatorButton symbol="(" type="op" expr={expression} SetExpr={SetExpression}/>
+          <OperatorButton symbol=")" type="op" expr={expression} SetExpr={SetExpression}/>
+          <SubmitButton expr={expression} SetResult={SetResult}/>
         </div>
         <br></br>
         <div id="numbers">
-          <Button symbol="7"></Button>
-          <Button symbol="8"></Button>
-          <Button symbol="9"></Button>
+          <NumberButton symbol="7" expr={expression} SetExpr={SetExpression}/>
+          <NumberButton symbol="8" expr={expression} SetExpr={SetExpression}/>
+          <NumberButton symbol="9" expr={expression} SetExpr={SetExpression}/>
           <br></br>
-          <Button symbol="4"></Button>
-          <Button symbol="5"></Button>
-          <Button symbol="6"></Button>
+          <NumberButton symbol="4" expr={expression} SetExpr={SetExpression}/>
+          <NumberButton symbol="5" expr={expression} SetExpr={SetExpression}/>
+          <NumberButton symbol="6" expr={expression} SetExpr={SetExpression}/>
           <br></br>
-          <Button symbol="1"></Button>
-          <Button symbol="2"></Button>
-          <Button symbol="3"></Button>
+          <NumberButton symbol="1" expr={expression} SetExpr={SetExpression}/>
+          <NumberButton symbol="2" expr={expression} SetExpr={SetExpression}/>
+          <NumberButton symbol="3" expr={expression} SetExpr={SetExpression}/>
           <br></br>
-          <Button symbol="0"></Button>
+          <NumberButton symbol="0" expr={expression} SetExpr={SetExpression}/>
         </div>
       </div>
     </>
   )
 }
 
-function Button({symbol, type})
+
+function SubmitButton({expr, SetResult})
 {
   return (
-    <button class={type}>{symbol}</button>
+    <button class="op res">=</button>
   )
 }
+
+function NumberButton({symbol, expr, SetExpr})
+{
+  return <Button symbol={symbol} expr={expr} type="num" SetExpr={SetExpr}/>
+}
+
+function OperatorButton({symbol, expr, SetExpr})
+{
+  return <Button symbol={symbol} type="op" expr={expr} SetExpr={SetExpr}/>
+}
+
+
+function Button({symbol, type, expr, SetExpr})
+{
+  return (
+    <button onClick={() => {SetExpr(expr + symbol); console.log(expr)}} class={type}>{symbol}</button>
+  )
+}
+
 
 export default App
